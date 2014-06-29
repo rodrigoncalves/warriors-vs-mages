2dde78f82384732bb56171583e4c51ef03209fcd
Thiago Ribeiro <thiagitosouza@hotmail.com>
2014-06-29 14:51:21 -0300

    Add validations and association : one user has many missions and one mission belogs to users

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,2 +1,7 @@
 class Mission < ActiveRecord::Base
+
+  belongs_to :user
+  validates :description,:reward,,:duration, presence: true
+  validates :description, length: { maximum: 200 }
+  validates :reward, length: { maximum: 10 }
 end
