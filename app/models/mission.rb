062fdbfee621472ff1c6b33686c973eb5c8e6364
Thiago Ribeiro <thiagitosouza@hotmail.com>
2014-06-29 15:17:17 -0300

    Add  foreing_key user_id in mission.rb

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,6 +1,6 @@
 class Mission < ActiveRecord::Base
 
-  belongs_to :user
+  belongs_to :user, :foreign_key => user_id, class_name: 'User'
   validates :description,:reward,,:duration, presence: true
   validates :description, length: { maximum: 200 }
   validates :reward, length: { maximum: 10 }
